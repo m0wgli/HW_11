2b0dcaf727a4154b1de755ef9fe93cc748b771c5
m0wgli <rodiontiutiunyk@gmail.com>
2023-04-22 04:04:37 +0300

    Create hw_11.py

---
+++ hw_11.py
@@ -0,0 +1,244 @@
+from collections import UserDict
+from datetime import datetime, date
+
+
+def input_error(func):
+    def inner(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return 'Not enough params. Try help'
+        except ValueError:
+            return 'Invalid value. Try again'
+        except KeyError:
+            return 'Contact not found. Try again'
+    return inner
+
+
+class Field:
+    def __init__(self, value=None):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not value.isdigit() or len(value) != 12:
+            raise ValueError('Phone number must be a 12-digit number')
+        self.__value = value
+
+
+
+
+class Birthday(Field):
+    def __init__(self, value=None):
+        self.__value = None
+        if value is not None:
+            self.value = value
+
+    @property
+    def value(self):
+        return self.__value.strftime('%d-%m-%Y')
+    
+    @value.setter
+    def value(self, value):
+        try:
+            self.__value = datetime.strptime(value, '%d-%m-%Y')
+        except ValueError:
+            raise ValueError('Birthday must be in "dd-mm-yyyy" format')
+        
+    def replace(self, year):
+        if self.__value:
+            self.__value = date(
+                self.__value.year, self.__value.month, self.__value.day)
+
+class Record:
+    def __init__(self, name, phone=None, birthday = None):
+        self.name = name
+        self.phones = [phone] if phone else None
+        self.birthday = birthday
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+        return f'Phone {phone} successfully added'
+
+    def change_phone(self, index, phone):
+        self.phones[index] = phone
+        return f'Phone {phone} successfully changed'
+
+    def delete_phone(self, phone):
+        self.phones.remove(phone)
+
+    def __str__(self) -> str:
+        return f'{str(self.name)} {", ".join([str(p) for p in self.phones])} {str(self.birthday)}'
+    
+    def set_birthday(self, birthday):
+        self.birthday = birthday
+
+    def get_birthday(self, birthday):
+        return self.birthday.value if self.birthday else None
+    
+    def days_to_bd(self):
+        if not self.birthday:
+            return None
+        today = date.today()
+        next_birthday = self.birthday.replace(year=today.year)
+        if next_birthday and next_birthday < today:
+            next_birthday = next_birthday.replace(year=today.year + 1)
+            if next_birthday.year - today.year > 1:
+                return None
+        if next_birthday:
+            days_to_birthday = (next_birthday - today).days
+            return days_to_birthday
+
+
+class AddressBook(UserDict):
+    start_iterate = 0
+
+    def add_record(self, record):
+        name = record.name.value
+        self.data[name] = record
+    
+    def iterator(self, page =2):
+        while True:
+            if self.start_iterate >= len(self.data):
+                break
+
+            yield list(self.data.values())[self.start_iterate:self.start_iterate + page]
+            self.start_iterate += page
+
+    def show_all(self):
+        for record_batch in self.iterator():
+            for record in record_batch:
+                print(record)
+
+
+contacts = AddressBook()
+
+
+def help(*args):
+    return '''
+    "hello", відповідає у консоль "How can I help you?"
+
+    "help" Викликає список доступних команд
+
+    "add ...". За цією командою бот зберігає у пам'яті (у словнику наприклад) новий контакт. Замість ... 
+користувач вводить ім'я та номер телефону, обов'язково через пробіл.
+
+    "change..." За цією командою бот зберігає в пам'яті новий номер телефону існуючого контакту. Замість ...
+ користувач вводить ім'я та індекс та новий номер телефону, обов'язково через пробіл.
+
+    "phone ...." За цією командою бот виводить у консоль номер телефону для зазначеного контакту. Замість ... 
+    користувач вводить ім'я контакту, чий номер треба показати.
+
+    "daystobd ..." Повертає кількість днів до наступного дня народження контакту
+
+    "show all". За цією командою бот виводить всі збереженні контакти з номерами телефонів у консоль.
+
+    "good bye", "close", "exit" по будь-якій з цих команд бот завершує свою роботу після того, як виведе у консоль "Good bye!".
+    '''
+
+
+def hello(*args):
+    return '''How can I help you?'''
+
+
+def exit(*args):
+    return '''Good Bye'''
+
+
+def no_command(*args):
+    return '''Unknown command, try again'''
+
+
+def show_all(*args):
+    return contacts.show_all()
+
+
+@input_error
+def add(*args):
+    name = Name(args[0])
+    phone = Phone(args[1])
+    birthday = Birthday(args[2])
+    rec = contacts.get(str(name))
+    if rec:
+        return rec.add_phone(phone)
+    record = Record(name, phone, birthday)
+    contacts.add_record(record)
+    days_to_bd = record.days_to_bd()
+    if days_to_bd:
+        print('{days_to_bd} until next birthday')
+    return f"Added <{name.value}> with phone <{phone.value}> and birthday <{birthday.value}>"
+
+
+@input_error
+def phone(*args):
+    name = Name(args[0])
+    rec = contacts.get(name.value)
+    if rec:
+        return rec.phones
+    return f'There are no phones with name {name}'
+
+
+def change(*args):
+    name = Name(args[0])
+    index = int(args[1])
+    new_phone = Phone(args[2])
+    rec = contacts.get(str(name))
+    if rec:
+        return rec.change_phone(index, new_phone)
+    return f'There are no phones with name {name}'
+
+
+COMMANDS = {help: 'help',
+            add: 'add',
+            exit: ['exit', 'close', 'good bye'],
+            hello: 'hello',
+            phone: 'phone',
+            change: 'change',
+            show_all: 'show all',
+            }
+
+
+def command_handler(text):
+    for command, kword in COMMANDS.items():
+        if isinstance(kword, str):
+            if text.lower().startswith(kword):
+                return command, text.replace(kword, '').strip().split()
+        elif isinstance(kword, list):
+            if text.strip().lower() in kword:
+                return command, None
+    return no_command, None
+
+
+def main():
+    print(help())
+    while True:
+        user_input = input('>>>')
+        command, data = command_handler(user_input)
+
+        print(command(*data))
+
+        if command == exit:
+            break
+
+
+if __name__ == '__main__':
+    main()
